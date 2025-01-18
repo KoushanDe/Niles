41aaaa000e88c5377cb65dd4cea235b136d24bc3
KoushanDe <koushanriki007@gmail.com>
2025-01-18 19:41:50 +0530

    Empty sceleton

---
+++ src/main/java/com/koushan/niles/NilesApplication.java
@@ -0,0 +1,13 @@
+package com.koushan.niles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koushan/niles/NilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koushan.niles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
